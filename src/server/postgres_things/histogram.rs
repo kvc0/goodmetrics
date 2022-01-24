e26d2cd34fb053c0d4f842f45bfa3c3e66ab1050
Kenny <3454741+WarriorOfWire@users.noreply.github.com>
2022-01-23 21:09:07 -0800

    rename histogram

--- src/server/postgres_things/histogram.rs
+++ src/server/postgres_things/histogram.rs
@@ -13,7 +13,7 @@ pub async fn get_or_create_histogram_type(connector: &mut PostgresConnector) -> 
             if let Some(dbe) = e.as_db_error() {
                 match dbe.code() {
                     &SqlState::UNDEFINED_OBJECT => {
-                        log::info!("Probably missing sequential_exponential_histogram type. Going to try to make it: {:?}", dbe);
+                        log::info!("Probably missing histogram type. Going to try to make it: {:?}", dbe);
                         drop(transaction);
 
                         let transaction = connector.use_connection().await?;
@@ -36,7 +36,7 @@ pub async fn get_or_create_histogram_type(connector: &mut PostgresConnector) -> 
 }
 
 async fn get_histogram_type(transaction: &Transaction<'_>) -> Result<Type, tokio_postgres::Error> {
-    match transaction.prepare("SELECT $1::sequential_exponential_histogram").await {
+    match transaction.prepare("SELECT $1::histogram").await {
         Ok(statement) => {
             let histogram_type = statement.params()[0].clone();
 
@@ -51,10 +51,10 @@ async fn get_histogram_type(transaction: &Transaction<'_>) -> Result<Type, tokio
 async fn create_histogram_type(transaction: &Transaction<'_>) -> Result<Type, tokio_postgres::Error> {
     match transaction.batch_execute(r#"
 -- Data type alias for readability
-create domain sequential_exponential_histogram as jsonb;
+create domain histogram as jsonb;
 
--- Shared function to glob up a sequential_exponential_histogram from a number column
-CREATE OR REPLACE FUNCTION sequential_exponential_histogram_accumulate(internal_state sequential_exponential_histogram, next_row double precision) RETURNS sequential_exponential_histogram
+-- Shared function to glob up a histogram from a number column
+CREATE OR REPLACE FUNCTION histogram_accumulate(internal_state histogram, next_row double precision) RETURNS histogram
 AS $fn$
 DECLARE
     bucket text;
@@ -74,8 +74,8 @@ BEGIN
 END;
 $fn$ LANGUAGE plpgsql STRICT IMMUTABLE PARALLEL SAFE;
 
--- Shared function to combine sequential_exponential_histograms
-CREATE OR REPLACE FUNCTION sequential_exponential_histogram_combine(internal_state sequential_exponential_histogram, next_row sequential_exponential_histogram) RETURNS sequential_exponential_histogram
+-- Shared function to combine histograms
+CREATE OR REPLACE FUNCTION histogram_combine(internal_state histogram, next_row histogram) RETURNS histogram
 AS $fn$
 DECLARE
     _key   text;
@@ -91,7 +91,7 @@ BEGIN
 END;
 $fn$ LANGUAGE plpgsql STRICT IMMUTABLE PARALLEL SAFE;
 
-CREATE OR REPLACE FUNCTION sequential_exponential_histogram_combine_inv(internal_state sequential_exponential_histogram, next_row sequential_exponential_histogram) RETURNS sequential_exponential_histogram
+CREATE OR REPLACE FUNCTION histogram_combine_inv(internal_state histogram, next_row histogram) RETURNS histogram
 AS $fn$
 DECLARE
     _key   text;
@@ -112,35 +112,35 @@ $fn$ LANGUAGE plpgsql STRICT IMMUTABLE PARALLEL SAFE;
 -- For downsampling a numeric row into a histogram
 CREATE OR REPLACE AGGREGATE accumulate_seh(double precision)
 (
-    sfunc = sequential_exponential_histogram_accumulate,
-    stype = sequential_exponential_histogram,
+    sfunc = histogram_accumulate,
+    stype = histogram,
     initcond = '{}',
-    combinefunc = sequential_exponential_histogram_combine,
+    combinefunc = histogram_combine,
     PARALLEL = SAFE
 );
 
 -- For dynamically combining histograms for presentation
-CREATE OR REPLACE AGGREGATE accumulate_seh(sequential_exponential_histogram)
+CREATE OR REPLACE AGGREGATE accumulate_seh(histogram)
 (
-    sfunc = sequential_exponential_histogram_combine,
-    stype = sequential_exponential_histogram,
-    mstype = sequential_exponential_histogram,
-    msfunc = sequential_exponential_histogram_combine,
-    minvfunc = sequential_exponential_histogram_combine_inv,
+    sfunc = histogram_combine,
+    stype = histogram,
+    mstype = histogram,
+    msfunc = histogram_combine,
+    minvfunc = histogram_combine_inv,
     initcond = '{}',
-    combinefunc = sequential_exponential_histogram_combine,
+    combinefunc = histogram_combine,
     PARALLEL = SAFE
 );
 
 
--- Pivots a sequential_exponential_histogram out to rows of | bucket int | count text | for convenient graphing in Grafana.
+-- Pivots a histogram out to rows of | bucket int | count text | for convenient graphing in Grafana.
 -- Used in Grafana like:
 -- select
 --   time_bucket('1m', time) as time,
 --   (buckets(accumulate_seh(some_column))).*  -- Can be numeric or a pre-downsampled SEH column
 -- from metrics_table
 -- group by 1 order by 1;
-CREATE OR REPLACE FUNCTION buckets( seh sequential_exponential_histogram ) RETURNS TABLE(bucket bigint, count bigint)
+CREATE OR REPLACE FUNCTION buckets( seh histogram ) RETURNS TABLE(bucket bigint, count bigint)
 AS $$
     select (a.each).key::bigint as bucket, (a.each).value::bigint as count from (select jsonb_each(seh) as each) a;
 $$ LANGUAGE SQL STRICT IMMUTABLE PARALLEL SAFE;
