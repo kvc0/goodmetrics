dd4e6e5cd05657c970f0ff6446d5f6ea20bc86ea
Kenny <3454741+WarriorOfWire@users.noreply.github.com>
2022-12-19 12:59:08 -0800

    try osx protoc workaround

---
+++ install_protoc_osx.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+set -e
+set -x
+
+VERSION=21.9
+
+ARCH=x86_64
+# ARCH=aarch_64
+
+OS=osx
+
+# sudo apt-get -y install zip tree
+
+mkdir temp
+pushd temp
+  curl -L https://github.com/protocolbuffers/protobuf/releases/download/v$VERSION/protoc-$VERSION-$OS-$ARCH.zip -o protoc.zip
+  unzip -o protoc.zip -d protoc
+  sudo mv protoc/bin/* /usr/local/bin/
+  sudo mv protoc/include/* /usr/local/include/
+popd
+rm -rf temp
+
+tree /usr/local/include/google
